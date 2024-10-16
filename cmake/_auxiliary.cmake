de30307806d376763cfd99e430ccc93294025eca
Nikita <sergeev_n_v@mail.ru>
2024-10-16 23:08:07 +0300

    Добавлены функция проверки существования директорий и функция поиска поддиректорий

--- cmake/_auxiliary.cmake
+++ cmake/_auxiliary.cmake
@@ -1,3 +1,5 @@
+cmake_minimum_required(VERSION 3.25)
+
 # Служебные функции cmake
 
 #[====[.rst:
@@ -14,15 +16,16 @@
         - (опционально) список взаимно исключающих флагов, где проверяется, что в пределах вызова исследуемой функции
                         максимум был использован один такой флаг.
 
-    В случае обнаружения ошибки данная функция прерывает работу и выводит соответствующее сообщение.
-    Функция рекурсивно проверяет сама себя, контролируя переданные в нее аргументы.
+    В случае обнаружения ошибки функция прерывает работу и выводит соответствующее сообщение.
+
+    Функция проверяет свою сигнатуру.
 
     **Функция**::
 
-     __check_parameters__(PREFIX prefix
-                          PARAMETERS par1 par2 ...
-                          [OPTIONAL_PARAMETERS optPar1 optPar2 ...]
-                          [EXCLUSIVE_FLAGS flag1 flag2 ...])
+     __check_parameters__(PREFIX <prefix>
+                          PARAMETERS <par1> <par2> ...
+                          [OPTIONAL_PARAMETERS <optPar1> <optPar2> ...]
+                          [EXCLUSIVE_FLAGS <flag1> <flag2> ...])
 
     **Аргументы**
 
@@ -32,7 +35,7 @@
     -     ``EXCLUSIVE_FLAGS`` - (опционально) Список взаимно исключающих флагов
 #]====]
 
-function(__checkParameters__)
+function(__check_parameters__)
 
     # Если это старт функции
     if(NOT DEFINED __SELF_CHECKING__)
@@ -41,13 +44,13 @@ function(__checkParameters__)
         set(__SELF_CHECKING__ True)
 
         # Задать префикс парсинга
-        set(__PARSING_PREFIX__ "__FUNCTION_PARAMETERS_CHECKING__")
+        set(__PARSING_PREFIX__ "__FUNCTION_PARAMETERS_CHECKING_PREFIX__")
 
         # Парсить аргументы функции (для всех проверок одного раза достаточно)
         cmake_parse_arguments("${__PARSING_PREFIX__}" "" "PREFIX" "PARAMETERS;OPTIONAL_PARAMETERS;EXCLUSIVE_FLAGS" "${ARGN}")
 
         # Запустить самопроверку (одноуровневая рекурсия)
-        __checkParameters__()
+        __check_parameters__()
 
         # Отметить завершение этапа самопроверки
         set(__SELF_CHECKING__ False)
@@ -122,5 +125,187 @@ function(__checkParameters__)
     if(DEFINED "${__FUNCTION_PREFIX__}_UNPARSED_ARGUMENTS")
         message(FATAL_ERROR "Присутствуют лишние параметры: ${${__FUNCTION_PREFIX__}_UNPARSED_ARGUMENTS}")
     endif()
+
+endfunction()
+
+#[====[.rst:
+
+    **Описание**
+
+    Функция предназначена для проверки существования директорий.
+
+    В качестве аргумента должны быть переданы пути к проверяемым директориям.
+
+    В случае обнаружения ошибки функция прерывает работу и выводит соответствующее сообщение.
+
+    Функция проверяет свою сигнатуру.
+
+    **Функция**::
+
+     __check_directories_exists__(DIRS <dir1> <dir2> ...)
+
+    **Аргументы**
+
+    - ``DIRS`` - Пути к проверяемым директориям
+
+#]====]
+
+function(__check_directories_exists__)
+
+    # Задать префикс парсинга
+    set(__PARSING_PREFIX__ "__DIRECTORIES_EXISTENCE_CHECKING_PREFIX__")
+
+    # Задать конфигурацию параметров парсинга
+    set(__MULTIPLE_VALUE_ARGS__ "DIRS")
+
+    # Парсить параметры функции
+    cmake_parse_arguments("${__PARSING_PREFIX__}" "" "" "${__MULTIPLE_VALUE_ARGS__}" "${ARGN}")
+
+    # Проверить параметры функции
+    __check_parameters__(PREFIX "${__PARSING_PREFIX__}" PARAMETERS "${__MULTIPLE_VALUE_ARGS__}")
+
+    # Для каждой директории
+    foreach(__DIR__ ${${__PARSING_PREFIX__}_DIRS})
+
+        # Проверить что директория существует
+        if (NOT IS_DIRECTORY "${__DIR__}")
+            message(FATAL_ERROR "Не существует директории: ${__DIR__}")
+        endif()
+
+    endforeach()
+
+endfunction()
+
+#[====[.rst:
+
+    **Описание**
+
+    Функция предназначена для поиска и формирования списка поддиректорий для заданной директории.
+    По умолчанию поиск будет осуществляться рекурсивно до нахождения директорий на всех уровнях вложенности.
+    Опционально можно задать максимальную глубину вложенности, до которой будет осуществляться поиск.
+    По умолчанию исходная директория также добавляется в итоговый список. Опционально это можно запретить.
+
+    В качестве аргументов должны быть переданы:
+        - исходная директория, для которой будет осуществлен поиск вложенных директорий;
+        - имя выходной переменной, в которую будет записан список найденных директорий;
+        - (опционально) уровень вложенности, до которого следует искать поддиректории, (не должен быть отрицательным)
+                        (0 - исходная директория, 1 - первый уровень поддиректорий, ...);
+        - (опционально) флаг запрета добавления исходной директории в выходной список.
+
+    Функция проверяет свою сигнатуру.
+
+    **Функция**::
+
+     __collect_subdirectories__(DIRECTORY <dir>
+                                OUT_VAR <out-var>
+                                [MAX_DEPTH <max-depth>]
+                                [NO_ROOT])
+
+    **Аргументы**
+
+    - ``DIRECTORY`` - Исходная директория
+    -   ``OUT_VAR`` - Выходная переменная
+    - ``MAX_DEPTH`` - (опционально) Максимальная глубина вложенности
+    -   ``NO_ROOT`` - (опционально) Не добавлять исходную директорию
+
+#]====]
+
+function(__collect_subdirectories__)
+
+    # Задать префикс парсинга
+    set(__PARSING_PREFIX__ "__SUBDIRECTORIES_COLLECTING_PREFIX__")
+
+    # Задать конфигурацию параметров парсинга
+    set(__FLAGS__ "NO_ROOT")
+    set(__ONE_VALUE_ARGS__ "DIRECTORY" "OUT_VAR")
+    set(__OPTIONAL_ONE_VALUE_ARGS__ "MAX_DEPTH")
+
+    # Парсить параметры функции
+    cmake_parse_arguments("${__PARSING_PREFIX__}" "${__FLAGS__}" "${__ONE_VALUE_ARGS__};${__OPTIONAL_ONE_VALUE_ARGS__}" "" "${ARGN}")
+
+    # Проверить параметры функции
+    __check_parameters__(PREFIX "${__PARSING_PREFIX__}" PARAMETERS "${__ONE_VALUE_ARGS__}" OPTIONAL_PARAMETERS "${__OPTIONAL_ONE_VALUE_ARGS__}")
+
+    # Взять исходную директорию из аргумента
+    set(__ROOT_DIR__ "${${__PARSING_PREFIX__}_DIRECTORY}")
+
+    # Проверить существование исходной директории
+    __check_directories_exists__(DIRS "${__ROOT_DIR__}")
+
+    # Задать переменную для записи всех найденных файлов и директорий
+    set(__RESULT__)
+
+    # В случае, если НЕ задана максимальная глубина
+    if(NOT DEFINED "${__PARSING_PREFIX__}_MAX_DEPTH")
+
+        # Найти на всех уровнях файлы с директориями
+        file(GLOB_RECURSE __SEARCH_RESULT__
+            LIST_DIRECTORIES true
+            ${__ROOT_DIR__}/*)
+
+        # Для всех найденных файлов с директориями
+        foreach(__ELEM__ ${__SEARCH_RESULT__})
+
+            # Запомнить директории
+            if (IS_DIRECTORY ${__ELEM__})
+                list(APPEND __RESULT__ ${__ELEM__})
+            endif()
+
+        endforeach()
+
+    else()
+
+        # Взять значение максимальной глубины из аргумента
+        set(__MAX_DEPTH__ "${${__PARSING_PREFIX__}_MAX_DEPTH}")
+
+        # Проверить значение максимальной глубины
+        if(NOT ${__MAX_DEPTH__} MATCHES "^[0-9]+$")
+            message(FATAL_ERROR "Значение MAX_DEPTH должно быть целым беззнаковым числом: ${__MAX_DEPTH__}")
+        endif()
+
+        # Если еще не конец
+        if(NOT ${__MAX_DEPTH__} EQUAL 0)
+
+            # Найти на этом уровне все файлы с директориями
+            file(GLOB __SEARCH_RESULT__
+                LIST_DIRECTORIES true
+                ${__ROOT_DIR__}/*)
+
+            # Уменьшить уровень глубины
+            math(EXPR __REDUCED_MAX_DEPTH__ "${__MAX_DEPTH__} - 1")
+
+            # Для всех найденных файлов с директориями
+            foreach(__ELEM__ ${__SEARCH_RESULT__})
+
+                # Если это директория
+                if (IS_DIRECTORY ${__ELEM__})
+
+                    # Рекурсивно запустить поиск директорий (включая ее саму как исходную) на следующем уровне
+                    __collect_subdirectories__(DIRECTORY "${__ELEM__}" OUT_VAR __CURRENT_OUT_VAR__ MAX_DEPTH "${__REDUCED_MAX_DEPTH__}")
+
+                    # Добавить найденные директории к результату
+                    list(APPEND __RESULT__ "${__CURRENT_OUT_VAR__}")
+
+                endif()
+
+            endforeach()
+
+        endif()
+
+    endif()
+
+    # Взять имя выходной переменной из аргумента
+    set(__OUT_VAR__ "${${__PARSING_PREFIX__}_OUT_VAR}")
+
+    # Записать в выходную переменную все собранные директории
+    set(${__OUT_VAR__} "${__RESULT__}")
+
+    # Если не было специального флага -> добавить исходную директорию в итоговый список
+    if(NOT ${__PARSING_PREFIX__}_NO_ROOT)
+        list(APPEND ${__OUT_VAR__} "${__ROOT_DIR__}")
+    endif()
+
+    # Вернуть значение выходной переменной
+    return(PROPAGATE ${__OUT_VAR__})
 
 endfunction()
